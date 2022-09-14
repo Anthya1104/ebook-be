4a4f307f205f83fc14f8b3f3f3b81538572eedd5
Yusa <anthya1104@gmail.co>
2022-09-14 14:35:25 +0800

    [feat]404 500 middlewares added

--- server.js
+++ server.js
@@ -16,8 +16,20 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use('/api/1.0/test',(req, res, next) => {
+app.use('/api/1.0/test', (req, res, next) => {
   res.send('Hello From BE');
+});
+
+// 404
+app.use((req, res, next) => {
+  res.status(404).send('404 Not Found');
+});
+
+// 500
+app.use((err, req, res, next) => {
+  console.error('錯誤處理', err);
+  console.error('path:', req.path);
+  res.status(500).json({ message: '請洽系統管理員' });
 });
 
 app.listen(port, () => {
