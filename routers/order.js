a711eb98beda4f24c954abf3c00cd0fec45f9e49
Yusa <anthya1104@gmail.co>
2022-09-26 15:14:33 +0800

    [feat]Order API

--- routers/order.js
+++ routers/order.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../utils/db');
 
 // -> /api/1.0/order/get-order
 router.get('/get-order', async (req, res, next) => {
-  res.json('連接到get-order');
+  // console.log(req.query.member_id);
+  let [data] = await pool.execute('SELECT * FROM user_order WHERE user_id = ?', [req.query.member_id]);
+  console.log('data in order', data);
+  res.json(data);
 });
 
 module.exports = router;
