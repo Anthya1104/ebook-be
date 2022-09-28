43b3ecab043b42e1118d72b8ae9422d4db9a6526
Aaron <ysjhs00189@gmail.com>
2022-09-28 18:24:12 +0800

    [update]

--- routers/order.js
+++ routers/order.js
@@ -6,17 +6,24 @@ const pool = require('../utils/db');
 router.get('/get-order', async (req, res, next) => {
   // console.log(req.query.member_id);
   if (req.query.order) {
-    let [detailData] = await pool.execute ('SELECT user_order_detail.*, product.book_name AS p_name, product.price, product.book_img, user_order.user_id, user_order.id AS o_id, user_order.date, user_order.status, member.name AS u_name, member.member_class, member.email, member.user_name, member.birthday, member.account, marketing.coupon_name AS c_name
-    FROM user_order_detail
-    JOIN member ON user_order_detail.user_id = member.id
-    JOIN product ON user_order_detail.product_id = product.id
-    JOIN user_order ON user_order_detail.order_id = user_order.id
-    JOIN marketing ON user_order_detail.coupon_id=marketing.id
-    WHERE order_id= ')
-    return res.json({ message: '抓詳細訂單' });
+    let [detailData] = await pool.execute(
+      'SELECT user_order_detail.*, product.*, user_order.* FROM user_order_detail JOIN product ON user_order_detail.product_id = product.id JOIN user_order ON user_order_detail.order_id = user_order.id WHERE user_order_detail.order_id= ? AND user_order_detail.user_id =?',
+      [req.query.order, req.query.member_id]
+    );
+    // console.log('query-order', detailData.length);
+    let totalItems = detailData.length;
+    let itemsPrice = detailData.map((value) => value.price).reduce((a, b) => a + b);
+    // console.log('itemPrice', itemsPrice);
+    return res.json({
+      priceDetail: {
+        totalItems,
+        itemsPrice,
+      },
+      detailData,
+    });
   }
   let [data] = await pool.execute('SELECT * FROM user_order WHERE user_id = ?', [req.query.member_id]);
-  console.log('data in order', data);
+  // console.log('data in order', data);
   res.json(data);
 });
 
