1979efbfbb3864db5628e9cb137b656bc517fa28
cocohsieh <qqqfangtwtw@gmail.com>
2022-09-29 12:34:52 +0800

    [upload]

--- routers/order.js
+++ routers/order.js
@@ -5,6 +5,7 @@ const pool = require('../utils/db');
 // -> /api/1.0/order/get-order
 router.get('/get-order', async (req, res, next) => {
   // console.log(req.query.member_id);
+  // const req.query.member_id =30
   let [data] = await pool.execute('SELECT * FROM user_order WHERE user_id = ?', [req.query.member_id]);
   console.log('data in order', data);
   res.json(data);
